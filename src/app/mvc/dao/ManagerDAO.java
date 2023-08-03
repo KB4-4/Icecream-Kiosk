04c39bd03b2b24d4bf44d803b580c465e1f9ecf7
김현지 <86637372+aacara@users.noreply.github.com>
2023-08-03 19:49:37 +0900

    Update ManagerDAO.java

--- src/app/mvc/dao/ManagerDAO.java
+++ src/app/mvc/dao/ManagerDAO.java
@@ -27,7 +27,7 @@ public interface ManagerDAO {
 	/**
 	 *  3. 모든 아이템 검색
 	 */
-	ItemDTO selectItemAll() throws SearchWrongException;
+	List<ItemDTO> selectItemAll() throws SearchWrongException;
 	
 	/**
 	 *  4. 인기 아이템 검색(top3)
