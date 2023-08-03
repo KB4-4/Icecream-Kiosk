9175260067c93c3a55cf427d0cbaa3d3cd1d170d
김현지 <86637372+aacara@users.noreply.github.com>
2023-08-03 19:49:23 +0900

    Update ManagerSuccessView.java

--- src/app/mvc/view/ManagerSuccessView.java
+++ src/app/mvc/view/ManagerSuccessView.java
@@ -2,15 +2,26 @@ package app.mvc.view;
 
 import java.util.List;
 
+import app.mvc.dto.ItemDTO;
 import app.mvc.dto.OrderDTO;
 
 public class ManagerSuccessView {
 
-	public static void selectPrint(List<OrderDTO> list) {
-		System.out.println("----Order LIST ("+list.size()+") 개 ------------------");
+	public static void selectOrderPrint(List<OrderDTO> list) {
+		System.out.println("----주문 내역 ("+list.size()+") 개 ------------------");
 		for(OrderDTO orderDto : list) {
 			System.out.println(orderDto);//board.toString()호출
 		}
-		
+	}
+	
+	public static void selectItemPrint(List<ItemDTO> list) {
+		System.out.println("---- 아이스크림 종류 ("+list.size()+") 개 ------------------");
+		for(ItemDTO itemDto : list) {
+			System.out.println(itemDto);//board.toString()호출
+		}
+	}
+	
+	public static void messagePrint(String message) {
+		System.out.println(message);
 	}
 }
