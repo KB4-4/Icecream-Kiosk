9f12cb9b96e3a035997f51a7fd7b032d474a635c
김현지 <86637372+aacara@users.noreply.github.com>
2023-08-03 18:48:18 +0900

    Create ManagerMainApp.java

---
+++ src/app/mvc/view/ManagerMainApp.java
@@ -0,0 +1,11 @@
+package app.mvc.view;
+
+public class ManagerMainApp {
+
+	public static void main(String[] args) {
+		System.out.println("***프로그램 시작합니다.***************");
+		ManagerView.managerMenuChoice();
+
+	}
+
+}
